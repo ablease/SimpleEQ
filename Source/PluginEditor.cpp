7edc2f605df863353c449fa46a7550167af8c9ea
Alex Blease <alexblease@gmail.com>
2025-02-24 13:16:11 +0000

    Add Sliders to GUI
    for:
    peakFreqSlider
    peakGainSlider
    peakQualitySlider
    loCutFreqSlider
    hiCutFreqSlider
    loCutSlopeSlider
    hiCutSlopeSlider
    
    using juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
            juce::Slider::TextEntryBoxPosition::NoTextBox

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,7 +15,13 @@ SimpleEQAudioProcessorEditor::SimpleEQAudioProcessorEditor (SimpleEQAudioProcess
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+
+    for (auto* comp : getComps())
+    {
+        addAndMakeVisible(comp);
+    }
+
+    setSize (600, 400);
 }
 
 SimpleEQAudioProcessorEditor::~SimpleEQAudioProcessorEditor()
@@ -37,4 +43,34 @@ void SimpleEQAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+
+    auto bounds = getLocalBounds();
+    auto resposneArea = bounds.removeFromTop(bounds.getHeight() * 0.33);
+
+    auto loCutArea = bounds.removeFromLeft(bounds.getWidth() * 0.33);
+    auto hiCutArea = bounds.removeFromRight(bounds.getWidth() * 0.5);; // half of whats left (another 1/3)
+
+    loCutFreqSlider.setBounds(loCutArea.removeFromTop(loCutArea.getHeight() * 0.5));
+    loCutSlopeSlider.setBounds(loCutArea);
+
+    hiCutFreqSlider.setBounds(hiCutArea.removeFromTop(hiCutArea.getHeight() * 0.5));
+    hiCutSlopeSlider.setBounds(hiCutArea);
+
+    peakFreqSlider.setBounds(bounds.removeFromTop(bounds.getHeight() * 0.33));
+    peakGainSlider.setBounds(bounds.removeFromTop(bounds.getHeight() * 0.5));
+    peakQualitySlider.setBounds(bounds);
+}
+
+std::vector<juce::Component*> SimpleEQAudioProcessorEditor::getComps()
+{
+    return
+    {
+        &peakFreqSlider,
+        &peakGainSlider,
+        &peakQualitySlider,
+        &loCutFreqSlider,
+        &hiCutFreqSlider,
+        &loCutSlopeSlider,
+        &hiCutSlopeSlider
+    };
 }
