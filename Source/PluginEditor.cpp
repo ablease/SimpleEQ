2926f60473c996ec8e998f865abb3c15ab7a43c8
Alex Blease <alexblease@gmail.com>
2025-02-27 21:45:15 +0000

    Refactor Peak Coefficient generation

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -38,12 +38,75 @@ SimpleEQAudioProcessorEditor::~SimpleEQAudioProcessorEditor()
 //==============================================================================
 void SimpleEQAudioProcessorEditor::paint (juce::Graphics& g)
 {
+    using namespace juce; // save us from typing juce:: everywhere
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.fillAll (Colours::black);
 
-    g.setColour (juce::Colours::white);
-    g.setFont (juce::FontOptions (15.0f));
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    auto bounds = getLocalBounds();
+    auto responseArea = bounds.removeFromTop(bounds.getHeight() * 0.33);
+
+    auto w = responseArea.getWidth();
+
+    auto& loCut = monoChain.get<ChainPositions::LoCut>();
+    auto& peak = monoChain.get<ChainPositions::Peak>();
+    auto& hiCut = monoChain.get<ChainPositions::HiCut>();
+
+    auto sampleRate = audioProcessor.getSampleRate();
+
+    std::vector<double> mags;
+
+    mags.resize(w);
+
+    for (int i = 0; i < w; i++)
+    {
+        double mag = 1.f;
+        auto freq = mapToLog10(double(i) / double(w), 20.0, 20000.0);
+
+        if (!monoChain.isBypassed<ChainPositions::Peak>())
+            mag *= peak.coefficients->getMagnitudeForFrequency(freq, sampleRate);
+
+        if (!loCut.isBypassed<0>())
+            mag *= loCut.get<0>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        if (!loCut.isBypassed<1>())
+            mag *= loCut.get<1>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        if (!loCut.isBypassed<2>())
+            mag *= loCut.get<2>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        if (!loCut.isBypassed<3>())
+            mag *= loCut.get<3>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+
+        if (!hiCut.isBypassed<0>())
+            mag *= hiCut.get<0>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        if (!hiCut.isBypassed<1>())
+            mag *= hiCut.get<1>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        if (!hiCut.isBypassed<2>())
+            mag *= hiCut.get<2>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        if (!hiCut.isBypassed<3>())
+            mag *= hiCut.get<3>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+
+        mags[i] = Decibels::gainToDecibels(mag);
+    }
+
+    Path responseCurve;
+
+    const double outputMin = responseArea.getBottom();
+    const double outputMax = responseArea.getY();
+    auto map = [outputMin, outputMax](double input)
+        {
+            return jmap(input, -24.0, 24.0, outputMin, outputMax);
+        };
+
+    responseCurve.startNewSubPath(responseArea.getX(), map(mags.front()));
+
+    for (size_t i = 1; i < mags.size(); i++)
+    {
+        responseCurve.lineTo(responseArea.getX() + i, map(mags[i]));
+    }
+
+    g.setColour(Colours::orange);
+    g.drawRoundedRectangle(responseArea.toFloat(), 4.f, 1.f);
+
+    g.setColour(Colours::white);
+    g.strokePath(responseCurve, PathStrokeType(2.f));
 }
 
 void SimpleEQAudioProcessorEditor::resized()
@@ -66,6 +129,20 @@ void SimpleEQAudioProcessorEditor::resized()
     peakFreqSlider.setBounds(bounds.removeFromTop(bounds.getHeight() * 0.33));
     peakGainSlider.setBounds(bounds.removeFromTop(bounds.getHeight() * 0.5));
     peakQualitySlider.setBounds(bounds);
+}
+
+void SimpleEQAudioProcessorEditor::parameterValueChanged(int parameterIndex, float newValue)
+{
+    parametersChanged.set(true);
+}
+
+void SimpleEQAudioProcessorEditor::timerCallback()
+{
+    if (parametersChanged.compareAndSetBool(false, true))
+    {
+        // update the monochain
+        // signal a repaint
+    }
 }
 
 std::vector<juce::Component*> SimpleEQAudioProcessorEditor::getComps()
